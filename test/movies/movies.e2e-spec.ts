1741bf8a4300c9690de354104dac9a70aa01d638
ddewaele <ddewaele@gmail.com>
2024-11-23 13:03:19 +0100

    Added additional tests

--- test/movies/movies.e2e-spec.ts
+++ test/movies/movies.e2e-spec.ts
@@ -3,7 +3,13 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { MoviesModule } from '../../src/movies/movies.module';
 import { mockClient } from 'aws-sdk-client-mock';
-import { DynamoDBDocumentClient, QueryCommand } from '@aws-sdk/lib-dynamodb';
+import {
+  DeleteCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+  QueryCommand,
+  UpdateCommand,
+} from '@aws-sdk/lib-dynamodb';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -24,12 +30,20 @@ describe('AppController (e2e)', () => {
   ];
 
   beforeEach(async () => {
-    // Mock the DynamoDB ScanCommand response
-    ddbMock.reset(); // Reset the mock before each test
+    ddbMock.reset();
     ddbMock.on(QueryCommand).resolves({
       Items: ALL_MOVIES,
     });
-
+    ddbMock.on(GetCommand).resolves({
+      Item: ALL_MOVIES[0],
+    });
+    ddbMock.on(DeleteCommand).resolves({});
+    ddbMock.on(UpdateCommand).resolves({
+      Attributes: {
+        ...ALL_MOVIES[0],
+        title: 'Updated Movie1',
+      },
+    });
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [MoviesModule],
     }).compile();
@@ -43,5 +57,35 @@ describe('AppController (e2e)', () => {
       .get('/movies')
       .expect(200)
       .expect(ALL_MOVIES);
+  });
+
+  it('/movies/:id (GET) - findById', () => {
+    return request(app.getHttpServer())
+      .get('/movies/MOVIE1')
+      .expect(200)
+      .expect(ALL_MOVIES[0]);
+  });
+
+  it('/movies/:id (DELETE) - deleteById', () => {
+    return request(app.getHttpServer())
+      .delete('/movies/MOVIE1')
+      .expect(200)
+      .expect({});
+  });
+
+  it('/movies/:id (PATCH) - update', () => {
+    const updatedMovie = {
+      title: 'Updated Movie1',
+      year: 2023,
+    };
+
+    return request(app.getHttpServer())
+      .patch('/movies/MOVIE1')
+      .send(updatedMovie)
+      .expect(200)
+      .expect({
+        ...ALL_MOVIES[0],
+        title: 'Updated Movie1',
+      });
   });
 });
