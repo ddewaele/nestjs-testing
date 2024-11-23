787cea148eb3cf107d02f096d3016313ee2aba74
ddewaele <ddewaele@gmail.com>
2024-11-23 11:02:07 +0100

    Logging

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,26 +1,32 @@
-import { Injectable } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Logger, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MoviesService {
+  private readonly logger = new Logger(MoviesService.name);
+
   create(createMovieDto: CreateMovieDto) {
-    return 'This action adds a new movie';
+    this.logger.log(`Creating a new movie with ${JSON.stringify(createMovieDto)}`);
+    return createMovieDto;
   }
 
   findAll() {
-    return `This action returns all movies`;
+    this.logger.log('Finding all movies');
+    return [];
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} movie`;
+    this.logger.log(`This action returns a #${id} movie`);
+    return {};
   }
 
   update(id: number, updateMovieDto: UpdateMovieDto) {
-    return `This action updates a #${id} movie`;
+    this.logger.log(`This action updates a #${id} movie`);
+    return updateMovieDto;
   }
 
   remove(id: number) {
-    return `This action removes a #${id} movie`;
+    this.logger.log(`This action removes a #${id} movie`);
   }
 }
